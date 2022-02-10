3cacaea0ad3af11595ed7d7aac810bb32a3bb29e
LES-TWINS <temogagna@gmail.com>
2022-02-11 03:22:14 +0400

    test

---
+++ script.js
@@ -0,0 +1,18 @@
+
+gsap.fromTo('.ang_icon',{opacity: 0 }, {opacity:1,duration:1.2},'<10%')
+gsap.fromTo('.html_icon',{opacity: 0 }, {opacity:1, duration:1.2},'<15%')
+gsap.fromTo('.css_icon',{opacity: 0}, {opacity:1, duration:1.2},'<15%')
+gsap.fromTo('.js_icon',{opacity: 0}, {opacity:1, duration:1.2},'<15%')
+gsap.fromTo('.python_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
+gsap.fromTo('.node_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
+gsap.fromTo('.c_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
+gsap.fromTo('.b_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
+
+
+// gsap.fromTo('.js_icon',{opacity: 0}, {opacity:1, duration:1.2},'<10%')
+// gsap.fromTo('.css_icon',{opacity: 0}, {opacity:1, duration:1.2},'<15%')
+// gsap.fromTo('.python_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
+// gsap.fromTo('.html_icon',{opacity: 0 }, {opacity:1, duration:1.2},'<15%')
+// gsap.fromTo('.node_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
+// gsap.fromTo('.ang_icon',{opacity: 0 }, {opacity:1,duration:1.2},'<15%')
+// gsap.fromTo('.c_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
