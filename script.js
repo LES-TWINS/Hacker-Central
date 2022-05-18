50cba033656eeb35d76d93b3e52ffb337a66fa3b
LES-TWINS <temogagna@gmail.com>
2022-05-18 15:56:26 +0400

    almost

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // gsap.fromTo('.ang_icon',{opacity: 0 }, {opacity:1,duration:1.2},'<10%')
 // gsap.fromTo('.html_icon',{opacity: 0 }, {opacity:1, duration:1.2},'<15%')
 // gsap.fromTo('.css_icon',{opacity: 0}, {opacity:1, duration:1.2},'<15%')
@@ -8,7 +7,6 @@
 // gsap.fromTo('.c_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
 // gsap.fromTo('.b_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
 
-
 // gsap.fromTo('.js_icon',{opacity: 0}, {opacity:1, duration:1.2},'<10%')
 // gsap.fromTo('.css_icon',{opacity: 0}, {opacity:1, duration:1.2},'<15%')
 // gsap.fromTo('.python_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
@@ -17,24 +15,48 @@
 // gsap.fromTo('.ang_icon',{opacity: 0 }, {opacity:1,duration:1.2},'<15%')
 // gsap.fromTo('.c_icon',{opacity: 0,}, {opacity:1, duration:1.2},'<15%')
 
-
-
 // gsap.fromTo('.input-group',{x:-100, opacity: 0,}, {opacity:1,x:0, duration:1.5},)
 
-let syl=document.querySelector('#syllabus');
-let course=document.querySelector('#course_box');
-let html=document.querySelector('#html');
-let ret=document.querySelector('#return_course')
-html.addEventListener('click',() => {
-    syl.style.display="block"
-    gsap.fromTo('#syllabus',{ opacity:0}, {opacity:1, duration:1},)
-    course.style.display="none"
-    
-})
-ret.addEventListener('click',() =>{
-   gsap.fromTo('#course_box',{opacity:0}, {opacity:1, duration:1},)
-    course.style.display="block";
-    syl.style.display="none";
 
-})
+
+
+
+let course = document.querySelector("#course_box");
+let hcou = document.querySelector("#first_cou");
+let ret = document.querySelector("#return_course");
+let join = document.querySelector("#join");
+let icbox = document.querySelector("#icon_box");
+let regbox = document.querySelector(".registration_card");
+let syl = document.querySelector("#syllabus");
+
+
+join?.addEventListener("click", () => {
+  icbox.style.display = "none";
+  gsap.fromTo(".registration_card", {y:-100, opacity: 0 }, { y:0, opacity: 1,  });
+  regbox.style.display = "flex";
+  document.body.style.backgroundColor="grey";
+ 
+});
+
+hcou?.addEventListener("click", () => {
+  syl.style.display = "block";
+  gsap.fromTo("#syllabus", { opacity: 0 }, { opacity: 1, duration: 1 });
+  course.style.display = "none";
+});
+
+ret?.addEventListener("click", () => {
+  gsap.fromTo("#course_box", { opacity: 0 }, { opacity: 1, duration: 1 });
+  course.style.display = "block";
+  syl.style.display = "none";
+});
+
+
+
+
+
+
+
+
+
+
 
