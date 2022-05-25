ceb263055cee6c35b40dd45b3ce459597474419d
LES-TWINS <temogagna@gmail.com>
2022-05-25 16:08:22 +0400

    burger menu

--- script.js
+++ script.js
@@ -21,35 +21,82 @@
 
 
 
-let course = document.querySelector("#course_box");
-let hcou = document.querySelector("#first_cou");
-let ret = document.querySelector("#return_course");
-let join = document.querySelector("#join");
-let icbox = document.querySelector("#icon_box");
-let regbox = document.querySelector(".registration_card");
-let syl = document.querySelector("#syllabus");
+// let course = document.querySelector("#course_box");
+// let hcou = document.querySelector("#first_cou");
+// let ret = document.querySelector("#return_course");
+// let join = document.querySelector("#join");
+// let icbox = document.querySelector("#icon_box");
+// let regbox = document.querySelector(".registration_card");
+// let syl = document.querySelector("#syllabus");
 
 
-join?.addEventListener("click", () => {
-  icbox.style.display = "none";
-  gsap.fromTo(".registration_card", {y:-100, opacity: 0 }, { y:0, opacity: 1,  });
-  regbox.style.display = "flex";
-  document.body.style.backgroundColor="grey";
+// join?.addEventListener("click", () => {
+//   icbox.style.display = "none";
+//   gsap.fromTo(".registration_card", {y:-100, opacity: 0 }, { y:0, opacity: 1,  });
+//   regbox.style.display = "flex";
+//   document.body.style.backgroundColor="grey";
  
-});
+// });
 
-hcou?.addEventListener("click", () => {
-  syl.style.display = "block";
-  gsap.fromTo("#syllabus", { opacity: 0 }, { opacity: 1, duration: 1 });
-  course.style.display = "none";
-});
+// hcou?.addEventListener("click", () => {
+//   syl.style.display = "block";
+//   gsap.fromTo("#syllabus", { opacity: 0 }, { opacity: 1, duration: 1 });
+//   course.style.display = "none";
+// });
 
-ret?.addEventListener("click", () => {
-  gsap.fromTo("#course_box", { opacity: 0 }, { opacity: 1, duration: 1 });
-  course.style.display = "block";
-  syl.style.display = "none";
-});
+// ret?.addEventListener("click", () => {
+//   gsap.fromTo("#course_box", { opacity: 0 }, { opacity: 1, duration: 1 });
+//   course.style.display = "block";
+//   syl.style.display = "none";
+// });
 
+
+
+const navSlide = () => {
+  const ae = document.querySelector(".ae");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+
+  burger.addEventListener("click", () => {
+    // Toggle Nav
+    if(ae.style.display=="none"){
+      ae.style.display="block";
+    } else{
+      ae.style.display="none"
+    }
+  
+    nav.classList.toggle("nav-active");
+     gsap.fromTo(".nav-links", {opacity:0}, {opacity:1});
+
+   
+ 
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+
+
+  //   if(ae.style.display=="none"){
+  //    ae.style.display="block";
+  //  } else{
+  //    ae.style.display="block";
+  //  }
+   
+    // Burger Animation
+    burger.classList.toggle('toggle')
+  });
+
+};
+
+navSlide();
 
 
 
